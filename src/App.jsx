6936f5a7b435f1a5de3bbb6ee6aefb222e8c5661
Alzhan-B <alzhan.braliyev@gmail.com>
2024-09-07 16:08:26 -0400

    Initial commit with Vite React setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+          <h1>This is My Portfolio</h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the links to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
