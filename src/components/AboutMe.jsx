3ba0dbd45ff44d62b4bd60e766ffa13b985c40bb
Alzhan-B <alzhan.braliyev@gmail.com>
2024-09-07 23:02:35 -0400

    Adding AboutMe

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,20 @@
+import "./AboutMe.css";
+
+const AboutMe = () => {
+  return (
+    <section className="about-me">
+      <h2>About Me</h2>
+      <p>
+        Hello! I’m Alzhan, an aspiring React developer passionate about
+        building engaging user interfaces.
+      </p>
+      <p>Skills: React, JavaScript, CSS, HTML</p>
+      <p>
+        I love working on projects that challenge me and help me grow as a
+        developer.
+      </p>
+    </section>
+  );
+};
+
+export default AboutMe;
